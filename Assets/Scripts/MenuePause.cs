7f138b547179fa9bf3dc5d70c06e4c3a0b41f8f6
Mikael.PC <mikaeljouchoux@gmail.com>
2018-01-25 15:19:31 +0100

    Script pause

---
+++ Assets/Scripts/MenuePause.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MenuePause : MonoBehaviour {
+
+    public GameObject menuePause; 
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        MenuePauseActive();
+        MenuePauseNotActive();
+	}
+    public void MenuePauseActive ()
+    {
+        menuePause.SetActive(true);
+        Time.timeScale = 0;
+    }
+    public void MenuePauseNotActive ()
+    {
+        menuePause.SetActive(false);
+        Time.timeScale = 1;
+    }
+}
