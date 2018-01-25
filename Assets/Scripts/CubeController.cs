98b2bc2188ea584ce0663c8d564acc250d7854a4
DESKTOP-S0E0I8R\Kévin <kevin.arenas@outlook.fr>
2018-01-25 14:10:42 +0100

    saut/ deplacement WIP

--- Assets/Scripts/CubeController.cs
+++ Assets/Scripts/CubeController.cs
@@ -5,14 +5,24 @@ using UnityEngine;
 public class CubeController: MonoBehaviour {
 
     private Rigidbody rb;
+    public float speed = 0 ;
+    public float jump = 0;
+    private bool canJump = true;
+   
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
 
-        //rb.AddForce(new Vector3(0, 0, ), ForceMode.Force);
-	}
-}
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector3(0, rb.velocity.y, -speed);
+
+        if ( Input.GetKeyDown(KeyCode.Space) && canJump = true) 
+            {
+            canJump = false;
+              rb.AddForce(transform.up * jump, ForceMode.Impulse);
+            }
+    }
+    }
