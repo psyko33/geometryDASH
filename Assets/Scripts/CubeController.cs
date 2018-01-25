a8ad0ea7d4a233997c29172bfdaa54618e7bea23
DESKTOP-S0E0I8R\Kévin <kevin.arenas@outlook.fr>
2018-01-25 10:46:06 +0100

    debut code

--- Assets/Scripts/CubeController.cs
+++ Assets/Scripts/CubeController.cs
@@ -2,15 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AvancerCube : MonoBehaviour {
+public class CubeController: MonoBehaviour {
 
+    private Rigidbody rb;
 	// Use this for initialization
 	void Start () {
-		
+        rb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+        //rb.AddForce(new Vector3(0, 0, ), ForceMode.Force);
 	}
 }
