017d108ba74ea2e4a61be15501dac8504f8ebc32
Mikael.PC <mikaeljouchoux@gmail.com>
2018-01-25 17:03:22 +0100

    ll

---
+++ Assets/Scripts/CameraF.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraF : MonoBehaviour
+{
+
+    public GameObject player;
+    private Vector3 distance;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        distance = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+
+        transform.position = player.transform.position + distance;
+
+    }
+}
